6ff9ad6d2bf4c53ebe860c9e75f932ce52505818
matthew-py <54901612+matthew-py@users.noreply.github.com>
2021-04-02 15:29:16 -0600

    update
    RPG Nested Dictionaries

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 # Matthew Deyenberg
 # computer science 30 period 1
-# 2021/03/16
+# 2021/04/02
 # rpg inventory
 # setting player name and making/showing lists
 player = []
 player = input('Whats your name: ')
-items = ['gun', 'poison']
-people = ['josh', 'luke', 'rick', 'joah', 'taylea', player]
-rooms = ['main floor', 'upstairs master bed room', 'basement']
+items = {'gun': {'bullets': "8"}, 'poison': '1'}
+people = {'josh': 'bystander', 'luke': 'bystander',
+          'rick': 'target', 'joah': 'bystander',
+          'taylea': 'bystander', player: 'assassin'}
+rooms = {'main floor': ['josh', 'luke'],
+         'upstairs master bed room': 'taylea', 'basement': ['rick', 'joah']}
 # setting varable for loops
 x = 0
 # showing the player whats avalible
 print("People at the party:")
-for players in people:
+for players, tags in people.items():
     x = x + 1
-    print(str(x)+'.', players)
+    print(str(x)+'.', players, ':', tags)
 # resetting x and printing
 x = 0
 print("Items")
-for item in items:
+for item, info in items.items():
     x = x + 1
-    print(str(x)+'.', item)
+    print(str(x)+'.', item, info)
 # resetting x again and printing
 x = 0
 print("Rooms")
-for room in rooms:
+for room, bystanders in rooms.items():
     x = x + 1
-    print(str(x)+'.', room)
+    print(str(x)+'.', room, ":", bystanders)
