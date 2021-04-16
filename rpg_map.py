f08b61e33d8015f1dada61a3d3c6bf88850cef4c
matthew-py <54901612+matthew-py@users.noreply.github.com>
2021-04-16 11:35:21 -0600

    rpg map
    basic rpg map

---
+++ rpg_map.py
@@ -0,0 +1,10 @@
+def map_of_floors():
+    '''prints a quick map'''
+    print('-Top Floor')
+    print('\tTaylea')
+    print("-Main Floor")
+    print("\tLuke")
+    print("\tJosh")
+    print('-Basement')
+    print("\tRick")
+    print("\tJoah")
